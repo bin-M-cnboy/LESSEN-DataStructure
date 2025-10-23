0d1e25e807179231be960e8230cab1a8026e7ef5
3766489095 <3766489095@qq.com>
2025-10-23 02:11:52 +0000

    1023-1

--- Lab2/Lab2/include/queue.h
+++ Lab2/Lab2/include/queue.h
@@ -12,26 +12,40 @@ class queue : public list<data_base> {
 
         bool empty() override {
             // TODO
-            return true;
+            return top_index == tail_index;
         }
+
         void pop() override {
             // TODO
+            if (!empty())
+            top_index = (top_index + 1) % MAX_LINK_LIST_NUM;
         }
+
         data_base top() override {
             // TODO
-            data_base tmp;
+            if (empty()) return data_base{};
+            
+            data_base tmp = list_data[top_index];
+            top_index = (top_index + 1) % MAX_LINK_LIST_NUM;
             return tmp;
         }
+
         void push(data_base push_data) override {
             // TODO
+        if (empty()) return;
+        
+        list_data[tail_index] = push_data;
+        tail_index = (tail_index + 1) % MAX_LINK_LIST_NUM;
         }
+
         void clear() override {
             // TODO
+            top_index = tail_index = 0;
         }
     private:
         int top_index = 0;
         int tail_index = 0;
         data_base list_data[MAX_LINK_LIST_NUM];
-};
+    };
 
 }
